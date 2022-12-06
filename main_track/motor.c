9a38575fea85f7a4ac4de3c0d53a13c7fb06f348
SheepBellDoor <sheepbelldoor.dev@gmail.com>
2022-12-06 18:00:46 +0900

    Modify: motor
    remove duplicated variable and add move to forward with distance parameter

--- main_track/motor.c
+++ main_track/motor.c
@@ -1,17 +1,171 @@
 #include "motor.h"
 
 
-uint32_t left_count = 0;
-uint32_t right_count = 0;
+uint32_t left_count_g = 0;
+uint32_t right_count_g = 0;
+
 // Function used to determine the turning radius.
 void TA3_N_IRQHandler(void) {
     TIMER_A3->CCTL[1] &= ~0x0001;
-    left_count++;
+    left_count_g++;
 }
 void TA3_0_IRQHandler(void) {
     TIMER_A3->CCTL[0] &= ~0x0001;
-    right_count++;
+    right_count_g++;
 }
+
+
+
+void rotate_left_30_degree(int speed) {
+    left_count_g = 0;
+    right_count_g = 0;
+    // rotation 30 degree
+    while(1) {
+        left_backward();
+        right_forward();
+        move(speed,speed);
+        if(left_count_g>60 && right_count_g>60) {
+            move(0,0);
+            break;
+        }
+    }
+}
+
+void rotate_right_30_degree(int speed) {
+    left_count_g = 0;
+    right_count_g = 0;
+    // rotation 30 degree
+    while(1) {
+        left_forward();
+        right_backward();
+        move(speed,speed);
+        if(left_count_g>60 && right_count_g>60) {
+            move(0,0);
+            break;
+        }
+    }
+}
+
+void rotate_left_45_degree(int speed) {
+    left_count_g = 0;
+    right_count_g = 0;
+    // rotation 45 degree
+    while(1) {
+        left_backward();
+        right_forward();
+        move(speed,speed);
+        if(left_count_g>90 && right_count_g>90) {
+            move(0,0);
+            break;
+        }
+    }
+}
+
+void rotate_right_45_degree(int speed) {
+    left_count_g = 0;
+    right_count_g = 0;
+    // rotation 30 degree
+    while(1) {
+        left_forward();
+        right_backward();
+        move(speed,speed);
+        if(left_count_g>90 && right_count_g>90) {
+            move(0,0);
+            break;
+        }
+    }
+}
+
+void rotate_left_60_degree(int speed) {
+    left_count_g = 0;
+    right_count_g = 0;
+    // rotate by 60 degree
+    while(1) {
+        left_backward();
+        right_forward();
+        move(speed,speed);
+        if(left_count_g>120 && right_count_g>120) {
+            move(0,0);
+            break;
+        }
+    }
+}
+
+void rotate_right_60_degree(int speed) {
+    left_count_g = 0;
+    right_count_g = 0;
+    // rotate by 60 degree
+    while(1) {
+        left_forward();
+        right_backward();
+        move(speed,speed);
+        if(left_count_g>120 && right_count_g>120) {
+            move(0,0);
+            break;
+        }
+    }
+}
+
+void rotate_left_90_degree(int speed) {
+    left_count_g = 0;
+    right_count_g = 0;
+    // rotate by 90 degree
+    while(1) {
+        left_backward();
+        right_forward();
+        move(speed,speed);
+        if(left_count_g>180 && right_count_g>180) {
+            move(0,0);
+            break;
+        }
+    }
+}
+
+void rotate_right_90_degree(int speed) {
+    left_count_g = 0;
+    right_count_g = 0;
+    // rotate by 90 degree
+    while(1) {
+        left_forward();
+        right_backward();
+        move(speed,speed);
+        if(left_count_g>180 && right_count_g>180) {
+            move(0,0);
+            break;
+        }
+    }
+}
+void rotate_left_degree(int speed, int degree) {
+    left_count_g = 0;
+    right_count_g = 0;
+    int count = degree * 2;
+    // rotate by degree 
+    while(1) {
+        left_backward();
+        right_forward();
+        move(speed,speed);
+        if(left_count_g>count && right_count_g>count) {
+            move(0,0);
+            break;
+        }
+    }
+}
+void rotate_right_degree(int speed, int degree) {
+    left_count_g = 0;
+    right_count_g = 0;
+    int count = degree * 2;
+    // rotate by degree 
+    while(1) {
+        left_forward();
+        right_backward();
+        move(speed,speed);
+        if(left_count_g>count && right_count_g>count) {
+            move(0,0);
+            break;
+        }
+    }
+}
+
 
 // Move function parameter is move speed.
 void move(uint16_t leftDuty, uint16_t rightDuty) {
@@ -33,28 +187,32 @@ void right_forward() {
 void right_backward() {
     P5->OUT |= 0x20;
 }
+/* 16.3702 is 1cm */
+void move_forward(int distance, uint16_t speed) {
 
-// It makes poppy move forward or stop, but doesn't work...
-void moveForward () {
-    P5->OUT &= ~0x30;
-    P2->OUT |= 0xC0;
     P3->OUT |= 0xC0;
-}
+    TIMER_A0->CCR[3] = speed;
+    TIMER_A0->CCR[4] = speed;
 
+    left_forward();
+    right_forward();
+    left_count_g = 0;
+    right_count_g = 0;
+
+    int dist = distance * 16.3702;
+
+    while(1) {
+        move(speed, speed);
+        if(left_count_g>dist && right_count_g>dist) {
+            move(0,0);
+            break;
+        }
+    }
+}
 void stop() {
     P2->OUT &= ~0xC0;
 }
 
-
-// UTILITY
-// Check rpm of poppy.
-uint32_t get_left_rpm() {
-    return 2000000/period_left;
-}
-
-uint32_t get_right_rpm() {
-    return 2000000/period_right;
-}
 
 // IRQ Handler
 // uint16_t first_left;
--- main_track/motor.h
+++ main_track/motor.h
@@ -1,12 +1,15 @@
 #ifndef __MOTOR_H__
 #define __MOTOR_H__
 
+#include "msp.h"
+#include "Clock.h"
+#include <stdio.h>
 #include <stdint.h>
 
 
 // ROTATE FUNCTION
-uint32_t left_count_g = 0;
-uint32_t right_count_g = 0;
+
+
 void TA3_N_IRQHandler(void);
 void TA3_0_IRQHandler(void);
 void rotate_left_30_degree(int speed);
@@ -17,6 +20,8 @@ void rotate_left_60_degree(int speed);
 void rotate_right_60_degree(int speed);
 void rotate_left_90_degree(int speed);
 void rotate_right_90_degree(int speed);
+void rotate_left_degree(int speed, int degree);
+void rotate_right_degree(int speed, int degree);
 
 
 // MOVE FUNCTION
@@ -25,13 +30,8 @@ void left_forward();
 void left_backward();
 void right_forward();
 void right_backward();
-
-// UITLITY
-void moveForward ();
+void move_forward(int distance, uint16_t speed); /* The unit of distance is centimeter */
 void stop();
-
-uint32_t get_left_rpm();
-uint32_t get_right_rpm();
 
 // etc...
 // uint16_t first_left;
