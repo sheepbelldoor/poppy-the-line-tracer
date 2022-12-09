93078b581cfac5330784c5bb9bb50293afc961a6
SheepBellDoor <sheepbelldoor.dev@gmail.com>
2022-12-09 17:10:26 +0900

    changed files
    nothing

--- main_track/Debug/subdir_vars.mk
+++ main_track/Debug/subdir_vars.mk
@@ -10,37 +10,49 @@ CMD_SRCS += \
 
 C_SRCS += \
 ../Clock.c \
+../init.c \
 ../main.c \
+../motor.c \
 ../startup_msp432p401r_ccs.c \
 ../system_msp432p401r.c 
 
 C_DEPS += \
 ./Clock.d \
+./init.d \
 ./main.d \
+./motor.d \
 ./startup_msp432p401r_ccs.d \
 ./system_msp432p401r.d 
 
 OBJS += \
 ./Clock.obj \
+./init.obj \
 ./main.obj \
+./motor.obj \
 ./startup_msp432p401r_ccs.obj \
 ./system_msp432p401r.obj 
 
 OBJS__QUOTED += \
 "Clock.obj" \
+"init.obj" \
 "main.obj" \
+"motor.obj" \
 "startup_msp432p401r_ccs.obj" \
 "system_msp432p401r.obj" 
 
 C_DEPS__QUOTED += \
 "Clock.d" \
+"init.d" \
 "main.d" \
+"motor.d" \
 "startup_msp432p401r_ccs.d" \
 "system_msp432p401r.d" 
 
 C_SRCS__QUOTED += \
 "../Clock.c" \
+"../init.c" \
 "../main.c" \
+"../motor.c" \
 "../startup_msp432p401r_ccs.c" \
 "../system_msp432p401r.c" 
 
