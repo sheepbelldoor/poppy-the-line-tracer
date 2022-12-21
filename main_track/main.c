08f458530b689a84ad78a9d2a5dc43158f7df9bb
SheepBellDoor <sheepbelldoor.dev@gmail.com>
2022-12-21 19:43:04 +0900

    Modify: main.c
    Change speed including weights and delete unused variable turn_count

--- main_track/main.c
+++ main_track/main.c
@@ -9,42 +9,43 @@
 void main(void){
     init_all();
 
-    int turn_count = 0; 
     int lap_count = 0;
 
+
+    
     while (lap_count < 2) {
         // 구간1 : 첫번째 센서 인식할때까지 직진 & 인식 후 90도 우회전
-        section1(1000, 1000, SPEED);
+        section1(2080, 2000, SPEED);
         // 구간2 : 첫번째 센서 인식할때까지 직진 & 인식 후 90도 우회전
-        section2(1000, 1000, SPEED);
+        section2(2080, 2000, SPEED);
         // 구간3 : 인식해도 회전 안함
-        section3(1000, 1000, SPEED);
+        section3(2080, 2000, SPEED);
         // 구간4 : 첫번째 센서 인식할때까지 직진 & 인식 후 90도 우회전
-        section4(1000, 1000, SPEED);
+        section4(2080, 2000, SPEED);
         // 구간5 : 마지막 센서 인식할때까지 직진 & 인식 후 90도 좌회전
-        section5(1000, 1000, SPEED);
+        section5(2080, 2000, SPEED);
         // 구간6 : 첫번째 센서 인식할때까지 직진 & 인식 후 90도 우회전
-        section6(1000, 1000, SPEED);
+        section6(2080, 2000, SPEED);
         // 구간7 : 마지막 센서 인식할때까지 직진 & 인식 후 90도 좌회전
-        section7(1000, 1000, SPEED);
+        section7(2080, 2000, SPEED);
         // 구간8 : 인식해도 회전 안함
-        section8(1000, 1000, SPEED);
+        section8(2080, 2000, SPEED);
         // 구간9 : 마지막 센서 인식할때까지 직진 & 인식 후 90도 좌회전
-        section9(1000, 1000, SPEED);
+        section9(2080, 2000, SPEED);
         // 구간10 : 마지막 센서 인식할때까지 직진 & 인식 후 90도 좌회전
-        section10(1000, 1000, SPEED);
+        section10(2080, 2000, SPEED);
         // 구간11 : line tracing 부분
-        section11(1000, 1000, SPEED);
+        section11(2080, 2000, SPEED);
         // 구간12 : 지그재그 부분
-        section12(1000, 1000, SPEED);
+        section12(2080, 2000, SPEED);
         // 구간13 : 마지막 센서 인식할때까지 직진 & 인식 후 90도 좌회전
-        section13(1000, 1000, SPEED);
+        section13(2080, 2000, SPEED);
         // 구간14 : 첫번째 센서 인식할때까지 직진 & 인식 후 90도 우회전
-        section14(1000, 1000, SPEED);
+        section14(2080, 2000, SPEED);
 
-        move_lap2(1000, 1000, SPEED);
-
-        //  시작 랩 인식
+        move_lap2(2080, 2000, SPEED, lap_count);
+         
+        // 시작 랩 인식
         lap_count++;
     }
     move(0,0);
